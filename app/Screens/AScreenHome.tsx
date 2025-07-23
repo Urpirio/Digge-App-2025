f84af0e1c881faaf7e1e0b827f60087e1a3ad542
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-23 15:39:59 -0400

    Se agrego la pantalla del Home con todos lo componentes

---
+++ app/Screens/AScreenHome.tsx
@@ -0,0 +1,19 @@
+import Section_AccionesRapidas from "@/Components/Components_ScreenHome/Components/Section_AccionesRapidas";
+import Section_ActividadReciente from "@/Components/Components_ScreenHome/Components/Section_ActividadReciente";
+import Section_ResumenActividad from "@/Components/Components_ScreenHome/Components/Section_ResumenActividad";
+import Section_User from "@/Components/Components_ScreenHome/Components/Section_User";
+import { ScrollView } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function ScreenHome() {
+  return (
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+      <Section_User />
+      <ScrollView>
+        <Section_ResumenActividad />
+        <Section_AccionesRapidas />
+        <Section_ActividadReciente/>
+      </ScrollView>
+    </SafeAreaProvider>
+  );
+}
