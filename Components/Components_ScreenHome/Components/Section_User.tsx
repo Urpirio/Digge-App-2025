8e5109b603ecca23d57ab6e4636ab473df33312f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-23 15:43:24 -0400

    Se agrego la seccion user de la pantalla Home

---
+++ Components/Components_ScreenHome/Components/Section_User.tsx
@@ -0,0 +1,46 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Card_Score from "../SubComponents/Card_Score";
+
+export default function Section_User() {
+  return (
+    <View>
+        <View
+        style={{
+          paddingTop: 30,
+          paddingBottom:20,
+          paddingHorizontal: 20,
+          justifyContent: "flex-end",
+          borderBottomRightRadius:30,
+          borderBottomLeftRadius:30,
+          backgroundColor:'#0F539C',
+          gap:20,
+          
+        }}
+      >
+        <View
+          style={{ flexDirection: "row", justifyContent: "flex-end", gap: 10 }}
+        >
+          <TouchableOpacity>
+            <Ionicons name="search-outline" size={30} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="notifications-outline" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flexDirection:'row',alignItems:'center',gap:10}}>
+          <Image
+          style={{height:60,width:60,borderRadius:80}}
+          source={{uri:'https://i.pinimg.com/736x/eb/76/a4/eb76a46ab920d056b02d203ca95e9a22.jpg'}}/>
+          <View>
+            <Text style={{fontSize:24,color:'white'}}>Hola, Juan!</Text>
+            <Text style={{color:'#ffffffa2'}}>Ciudadano</Text>
+          </View>
+        </View>
+
+        <Card_Score/>
+      </View>
+    </View>
+  )
+}
