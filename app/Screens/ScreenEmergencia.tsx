dec84f74774c0029b26eeb9eb0ccb53f40efda19
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-23 15:41:55 -0400

    Se agrego la base pantalla de emergencia

---
+++ app/Screens/ScreenEmergencia.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function ScreenEmergencia() {
+  return (
+    <SafeAreaProvider>
+        
+    </SafeAreaProvider>
+  )
+}
