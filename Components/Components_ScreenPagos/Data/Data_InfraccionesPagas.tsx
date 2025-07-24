8b9f6e0c5b05ab1b2d4d0ecd00262cc629a40d06
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 17:36:49 -0400

    Se agrego el array de datos de la pantalla de pagos esto para las cartas de infracciones pagadas

---
+++ Components/Components_ScreenPagos/Data/Data_InfraccionesPagas.tsx
@@ -0,0 +1,30 @@
+
+export const Data_InfraccionesPagas = [
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+    },
+]
