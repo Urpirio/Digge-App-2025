f8331758f976a8da7000410f9e0f091fb314fe50
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 17:37:27 -0400

    Se agrego el hook de las card de pagos pendientes

---
+++ Components/Components_ScreenPagos/hooks/useInfraccionesPendientes.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { View, Text } from "react-native";
+
+export const useInfraccionesPendientes = () => {
+  const [StatusContainerPagar, setStatusContainerPagar] = useState(false);
+
+  const StatusInfracciones = (D: { Status: String }) => {
+    switch (D.Status) {
+      case "Media":
+        return (
+          <View
+            style={{
+              paddingHorizontal: 10,
+              paddingVertical:2,
+              borderRadius: 8,
+              backgroundColor: "#ff99002d",
+            }}
+          >
+            <Text style={{ color: "#FF9900" }}>Media</Text>
+          </View>
+        );
+      case "Regular":
+        return (
+          <View
+            style={{
+              paddingHorizontal: 10,
+              borderRadius: 8,
+              backgroundColor: "#15803c30",
+              paddingVertical:2
+            }}
+          >
+            <Text style={{ color: "#15803D" }}>Regular</Text>
+          </View>
+        );
+      case "Alta":
+        return (
+          <View
+            style={{
+              paddingHorizontal: 10,
+              paddingVertical:2,
+              borderRadius: 8,
+              backgroundColor: "#ee2b243b",
+            }}
+          >
+            <Text style={{ color: "#EE2A24" }}>Alta</Text>
+          </View>
+        );
+    }
+  };
+
+  return {
+    StatusContainerPagar,
+    setStatusContainerPagar,
+    StatusInfracciones,
+  };
+};
