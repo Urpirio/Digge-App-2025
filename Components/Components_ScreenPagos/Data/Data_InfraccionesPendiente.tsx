2e83a9bccdb83350391316f45dd060f837f69c2b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 17:37:13 -0400

    Se agrego el array de datos para las card pendientes de la pantalla de pago

---
+++ Components/Components_ScreenPagos/Data/Data_InfraccionesPendiente.tsx
@@ -0,0 +1,36 @@
+
+export const Data_InfraccionesPendiente = [
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'Alta',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+        PrecioMulta: 'RD$1,000'
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'Media',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+        PrecioMulta: 'RD$1,500'
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'Regular',
+        fechaPago: 'Pagada el 20 de julio de 2025',
+        PrecioMulta: 'RD$2,200'
+    },
+]
