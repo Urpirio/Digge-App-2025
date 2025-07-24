0ce51368115004b9cced1210a3045ca5fda32c41
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:10:42 -0400

    Se agrego el array de datos de las matricula y datos de los carros

---
+++ Components/Components_Vehiculos/Data/Data_CardMatriculasCarros.ts
@@ -0,0 +1,23 @@
+
+
+export const Data_CardMatriculasCarros = [
+    {
+        TipoAuto:'Sedán',
+        Matricula: 'A849201',
+        Ano:'2019',
+        Modelo:'Toyota Corolla'
+
+    },
+    {
+        TipoAuto:'Jipeta',
+        Matricula: 'G527394',
+        Ano:'2021',
+        Modelo:'Hyundai Santa Fe'
+    },
+    {
+        TipoAuto:'Camioneta',
+        Matricula: 'L302188',
+        Ano:'2020',
+        Modelo: 'Mitsubishi L200',  
+    }
+]
