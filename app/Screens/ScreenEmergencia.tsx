2a7d6989e6e0009c1254322aa9ff9739b4e9c598
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 17:33:18 -0400

    Se agrego la pantalla de Emergencia y sus respectivas secciones

--- app/Screens/ScreenEmergencia.tsx
+++ app/Screens/ScreenEmergencia.tsx
@@ -1,9 +1,14 @@
+import Section_ContactosEmergencia from "@/Components/Components_ScreenEmergencia/Components/Section_ContactosEmergencia";
+import Section_Emergencia from "@/Components/Components_ScreenEmergencia/Components/Section_Emergencia";
+import Section_Header from "@/Components/Components_ScreenEmergencia/Components/Section_Header";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function ScreenEmergencia() {
   return (
-    <SafeAreaProvider>
-        
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+        <Section_Header/>
+        <Section_Emergencia/>
+        <Section_ContactosEmergencia/>
     </SafeAreaProvider>
   )
 }
