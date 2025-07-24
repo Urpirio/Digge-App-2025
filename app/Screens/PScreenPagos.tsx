348ef453f6bb958fda95433ff242758634513bb6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 17:33:00 -0400

    Se agrego la pantalla de pagos y sus repectivas secciones

--- app/Screens/PScreenPagos.tsx
+++ app/Screens/PScreenPagos.tsx
@@ -1,9 +1,15 @@
+import Section_Header from "@/Components/Components_ScreenPagos/Components/Section_Header";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useState } from "react";
+import Section_Dashboard from "@/Components/Components_ScreenPagos/Components/Section_Dashboard";
+import Section_Historial from "@/Components/Components_ScreenPagos/Components/Section_Historial";
 
 export default function ScreenPagos() {
+  const [StateSelect,setStateSelect] = useState(false)
   return (
-    <SafeAreaProvider>
-        
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+        <Section_Header StateSelect={StateSelect} setStateSelect={setStateSelect}/>
+        {StateSelect ? <Section_Historial/> : <Section_Dashboard/>}
     </SafeAreaProvider>
   )
 }
