b9e4abec64c5ece94fe8d7e94b765cb70bb9b711
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:04:19 -0400

    Se agrego el array de datos de las infracciones

---
+++ Components/Components_ScreenDenuncias/Data/Data_Infraccion.ts
@@ -0,0 +1,30 @@
+
+export const Data_Infraccion = [
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'Procesando',
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'No procesable',
+    },
+    {
+        num_infraccion: 'IFR-2025-001',
+        titulo:'Exceso de Velocidad',
+        subtitulo:'Exceso de velocidad en zona escolar ',
+        matricula:'A123456',
+        ubicacion:'Av. Winston Churchill ',
+        hora: '08:30 AM',
+        status: 'Procesado',
+    }
+];
