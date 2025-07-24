d529f2f5576224e4a5c70c8bf596c84ef7dac09b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:08:34 -0400

    Se hicieron mejoras en estilo

--- Components/Components_ScreenHome/Components/Section_ActividadReciente.tsx
+++ Components/Components_ScreenHome/Components/Section_ActividadReciente.tsx
@@ -7,7 +7,7 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function Section_ActividadReciente() {
   return (
-    <View style={{ paddingBottom: 100, paddingTop: 10,paddingHorizontal:20 }}>
+    <View style={{ paddingBottom: 100, paddingTop: 10,paddingHorizontal:10 }}>
         <Text style={{ fontSize: 20, fontWeight: "600" }}>Actividad reciente</Text>
         <View style={{paddingVertical:10,gap:10}}>
             
