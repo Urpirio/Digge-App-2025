1691aed51ab4b7e7d2111d2d6a089a50225762ad
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:10:23 -0400

    Se agrego la seccion de Tu carros a la pantalla  Vehiculos

---
+++ Components/Components_Vehiculos/Components/Section_TusCarros.tsx
@@ -0,0 +1,22 @@
+import { View } from "react-native";
+import Card_MatriculasTusCarros from "../SubComponents/Card_MatriculasTusCarros";
+import Card_AgregarCarros from "../SubComponents/Card_AgregarCarros";
+
+export default function Section_TusCarros() {
+  return (
+    <View style={{ paddingHorizontal: 10, paddingVertical: 10, gap: 20 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          gap: 12,
+        }}
+      >
+        <Card_MatriculasTusCarros />
+        <Card_AgregarCarros/>
+      </View>
+
+    </View>
+  );
+}
