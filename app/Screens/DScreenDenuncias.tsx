6ad1c347b6ed8d126bdd1e37c60116243503a480
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:02:19 -0400

    Se agregaron las nuevas secciones terminas a la pantalla de Denuncias

--- app/Screens/DScreenDenuncias.tsx
+++ app/Screens/DScreenDenuncias.tsx
@@ -1,9 +1,19 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { useState } from "react";
+import Section_Header from "@/Components/Components_ScreenDenuncias/Components/Section_Header";
+import Section_Dashboard from "@/Components/Components_ScreenDenuncias/Components/Section_Dashboard";
+import Section_Historial from "@/Components/Components_ScreenDenuncias/Components/Section_Historial";
 
 export default function ScreenDenuncias() {
+
+  const [StateSelect,setStateSelect] = useState(false);
+
+
   return (
-    <SafeAreaProvider>
-        
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+      <Section_Header StateSelect={StateSelect} setStateSelect={setStateSelect}/>
+      {StateSelect ? <Section_Historial/> : <Section_Dashboard/>}
     </SafeAreaProvider>
-  )
+  );
 }
