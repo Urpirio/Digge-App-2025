07329c94ea8297f8d2c92dd4d4fefb70abfb7e64
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:02:12 -0400

    Se agregaron las nuevas secciones terminas a la pantalla de vehiculos

--- app/Screens/BScreenVehiculos.tsx
+++ app/Screens/BScreenVehiculos.tsx
@@ -1,9 +1,14 @@
+import Section_header from "@/Components/Components_Vehiculos/Components/Section_header";
+import Section_Principal from "@/Components/Components_Vehiculos/Components/Section_Principal";
+import Section_TusCarros from "@/Components/Components_Vehiculos/Components/Section_TusCarros";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function ScreenVehiculos() {
   return (
-    <SafeAreaProvider>
-        
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+        <Section_header/>
+        <Section_Principal/>
+        <Section_TusCarros/>
     </SafeAreaProvider>
   )
 }
