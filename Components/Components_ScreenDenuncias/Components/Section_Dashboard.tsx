fdcee55ac955a515c41bfe91dfbbe8b2e2dc6aa7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-24 11:02:55 -0400

    Se agrego la seccion de Dashboard para la denucias

---
+++ Components/Components_ScreenDenuncias/Components/Section_Dashboard.tsx
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+import Card_CrearDenuncia from "../SubComponents/Card_CrearDenuncia";
+import Card_Varibles from "../SubComponents/Card_Varibles";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Feather from "@expo/vector-icons/Feather";
+
+export default function Section_Dashboard() {
+  return (
+    <View style={{ paddingHorizontal: 10, paddingVertical: 10, gap: 10 }}>
+      <Card_CrearDenuncia />
+
+      <Card_Varibles
+        Icon={<Ionicons size={25} name="car-outline" color={"white"} />}
+        Titulo={"Denunciar Conductor"}
+        subTitulo={"Reporta un conductor"}
+        bgIconColor={"FF9700"}
+      />
+
+      <Card_Varibles
+        Icon={<Feather name="tool" size={24} color="white" />}
+        Titulo={"Reportar Avería"}
+        subTitulo={"Daños en vías"}
+        bgIconColor={"4A739C"}
+      />
+    </View>
+  );
+}
