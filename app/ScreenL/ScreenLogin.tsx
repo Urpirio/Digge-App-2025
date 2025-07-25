14b13a12723cc0c584ba0abbdc0e9198caf2d69a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-25 06:00:39 -0400

    Se agregaron los inputs yse hicieron mejoras en el estilo de los mismos y tambien se agrego un hook

--- app/ScreenL/ScreenLogin.tsx
+++ app/ScreenL/ScreenLogin.tsx
@@ -1,7 +1,15 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Image, View ,Text, TextInput, TouchableOpacity} from "react-native";
+import { Image, View, Text, TextInput, TouchableOpacity, StatusBar } from "react-native";
+import { router } from "expo-router";
+import { useEnviarDatos } from "@/Components/Components_Login/hooks/useEnviarDatos";
+import { useStyleLogin } from "@/Components/Components_Login/hooks/useStyleLogin";
+import { useEffect } from "react";
 
 export default function ScreenLogin() {
+  const { InputEmail, setInputEmail, InputPass, setInputPass } =
+    useEnviarDatos();
+  const { IconPass, setIconPass, BtnIconPass } = useStyleLogin();
+
   return (
     <SafeAreaProvider>
       <Image
@@ -13,7 +21,8 @@ export default function ScreenLogin() {
           position: "absolute",
           height: "100%",
           width: "100%",
-          justifyContent: "center",
+          paddingTop: 80,
+          paddingHorizontal: 20,
           gap: 30,
         }}
       >
@@ -33,14 +42,57 @@ export default function ScreenLogin() {
             Inicia sesión en tu cuenta
           </Text>
         </View>
-        <View>
-          <TextInput/>
-          <TextInput/>
-          <TouchableOpacity>
-            <Text>Iniciar sesion</Text>
-          </TouchableOpacity>
+        <View style={{ gap: 10 }}>
+          <TextInput
+            value={InputEmail}
+            onChangeText={setInputEmail}
+            placeholder="Correo electrónico "
+            placeholderTextColor={"#adb5bd"}
+            style={{
+              borderWidth: 1,
+              backgroundColor: "#f5f3f3ff",
+              fontSize: 16,
+              borderRadius: 10,
+              borderColor: "#e9ecef",
+              height: 50,
+            }}
+          />
+          <View
+            style={{
+              borderWidth: 1,
+              backgroundColor: "#f5f3f3ff",
+              flexDirection: "row",
+              justifyContent:'space-between',
+              borderRadius: 10,
+              borderColor: "#e9ecef",
+              alignItems:'center',
+              padding:5
+            }}
+          >
+            <TextInput
+              value={InputPass}
+              onChangeText={setInputPass}
+              showSoftInputOnFocus
+              secureTextEntry={IconPass}
+              placeholder="Contraseña"
+              placeholderTextColor={"#adb5bd"}
+              style={{ fontSize: 16,width:'90%' }}
+            />
+            <TouchableOpacity onPress={()=>setIconPass(!IconPass)}>
+              <BtnIconPass />
+            </TouchableOpacity>
+          </View>
         </View>
+        <TouchableOpacity
+          onPress={() => router.navigate("/Screens/AScreenHome")}
+          style={{ padding: 12, backgroundColor: "#0F539C", borderRadius: 10 }}
+        >
+          <Text style={{ textAlign: "center", color: "white", fontSize: 18 }}>
+            Iniciar sesion
+          </Text>
+        </TouchableOpacity>
       </View>
+      <StatusBar barStyle={'dark-content'}/>
     </SafeAreaProvider>
   );
 }
