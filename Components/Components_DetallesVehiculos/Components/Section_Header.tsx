71c1f1c8168889b5e5de54c7b35b84bd441471f2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-27 19:46:41 -0400

    Se agrego la seccion del heade de la pantalla de detalles

---
+++ Components/Components_DetallesVehiculos/Components/Section_Header.tsx
@@ -0,0 +1,26 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import { router } from "expo-router";
+
+export default function Section_Header() {
+  return (
+    <View
+      style={{
+        paddingHorizontal: 10,
+        paddingTop: 40,
+        paddingBottom: 10,
+        backgroundColor:'#0F539C'
+      }}
+    >
+      <TouchableOpacity 
+      onPress={()=>router.back()}
+      style={{flexDirection:'row',alignItems:'center',gap:5}}>
+        <Feather name="arrow-left" size={28} color="#f8f9faa7" />
+        <Text style={{color:'#f8f9faa7'}}>Volver</Text>
+      </TouchableOpacity>
+      <View>
+        <Text style={{fontSize:24,fontWeight:'300',color:'white'}}>Detalles de vehículo</Text>
+      </View>
+    </View>
+  );
+}
