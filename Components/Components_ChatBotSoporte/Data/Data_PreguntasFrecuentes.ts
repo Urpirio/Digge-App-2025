d9cdd4e376152a944be55b3e8c55f694e0f644af
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-27 17:38:38 -0400

    Se agrego un array de datos para las preguntas predeterminadas

---
+++ Components/Components_ChatBotSoporte/Data/Data_PreguntasFrecuentes.ts
@@ -0,0 +1,15 @@
+
+export const Data_PreguntasFrecuentes = [
+    {
+        Pregunta: '¿Pregunta usualmente muy frecuente?',
+    },
+    {
+        Pregunta: '¿Como saco mi licencia de conducir?',
+    },
+    {
+        Pregunta: 'Como renuevo mi licencia de conducir?',
+    },
+    {
+        Pregunta: 'Como reporto una emergencia?',
+    }
+]
