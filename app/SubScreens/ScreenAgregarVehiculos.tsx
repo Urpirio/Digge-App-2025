15fb1d9a52c75547a767b8becaeef17336f17249
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-27 22:18:19 -0400

    Se agrego la pantalla de agregar vehiculo

--- app/SubScreens/ScreenAgregarVehiculos.tsx
+++ app/SubScreens/ScreenAgregarVehiculos.tsx
@@ -1,9 +1,12 @@
-import { View } from "react-native";
+import Section_Header from "@/Components/Components_AgregarVehiculo/Components/Section_Header";
+import Section_InformacionVehiculo from "@/Components/Components_AgregarVehiculo/Components/Section_InformacionVehiculo";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function ScreenAgregarVehiculos() {
   return (
-    <View>
-        
-    </View>
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+      <Section_Header/>
+      <Section_InformacionVehiculo/>
+    </SafeAreaProvider>
   )
 }
