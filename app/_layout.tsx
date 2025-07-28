817c1cc9231159e942d165b06f53076db4d6675a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-28 13:57:43 -0400

    Se agregaron nuevo header para el modal de reportes y camara

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,6 +34,15 @@ export default function _layout() {
           
         }}
       />
+      <Stack.Screen name="Mod/Modal_MapReportesAverias" options={{
+        presentation:'transparentModal',
+        animation:'slide_from_bottom'
+      }}/>
+      <Stack.Screen name="SubScreens/ScreenCamaraVideo" options={{
+        header:()=>{
+          return <View style={{height:40,backgroundColor:'#0F539C'}}/>
+        }
+      }}/>
     </Stack>
   );
 }
