c95a133a99f236d3c088ed3b4c4ea646944d4bf6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-28 14:05:21 -0400

    Se agrego el hook del formulario de la pantalla de Reportes

---
+++ Components/Components_ReportarAveria/Hook/useFormulario.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { PermissionsAndroid } from "react-native";
+import * as Location from "expo-location";
+import { getCurrentPositionAsync } from "expo-location";
+import { GoogleMaps } from "expo-maps";
+
+
+export const useFormulario = () => {
+  const [DropDownValue, setDropDownValue] = useState<string>();
+  const [InputCarriles, setInputCarriles] = useState<string>();
+  const [TextTareaDescripcion, setTextTareaDescripcion] = useState<string>();
+  const [Photos, setPhotos] = useState<any>();
+  const [Videos, setVideos] = useState<any>();
+  const [Localizacion, setLocalizacion] = useState<string | null>(
+    "Ubicación actaul (GPS)"
+  );
+
+  const LocationUser = async () => {
+    // Revisar mas adelante, esto es para la geolocalizacion del usuario
+    await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    );
+    let DataLocalizacion = await Location.requestBackgroundPermissionsAsync();
+    console.log(DataLocalizacion);
+
+   
+    
+
+
+    
+  };
+
+  return {
+    DropDownValue,
+    setDropDownValue,
+    InputCarriles,
+    setInputCarriles,
+    TextTareaDescripcion,
+    setTextTareaDescripcion,
+    Photos,
+    setPhotos,
+    Videos,
+    setVideos,
+    Localizacion,
+    setLocalizacion,
+    LocationUser,
+  };
+};
