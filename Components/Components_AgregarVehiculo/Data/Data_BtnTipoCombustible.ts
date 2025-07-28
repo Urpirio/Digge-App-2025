2a45322f3da721b453ce9669ed3787993bc16398
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-27 22:19:44 -0400

    Se agrego un array de datos con los titulos de cada tipo de combustble

---
+++ Components/Components_AgregarVehiculo/Data/Data_BtnTipoCombustible.ts
@@ -0,0 +1,19 @@
+
+
+export const Data_BtnTipoCombustible = [
+  {
+    titulo: "Gasolina",
+  },
+  {
+    titulo: "Diésel",
+  },
+  {
+    titulo: "Eléctrico",
+  },
+  {
+    titulo: "GLP",
+  },
+  {
+    titulo: "Híbrido",
+  },
+];
