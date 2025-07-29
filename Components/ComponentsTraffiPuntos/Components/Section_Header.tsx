ccdabb64e883a7e0f57e7867722e748894f8a9cb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-28 22:04:55 -0400

    Se agrego la seccion del header de la pantalla de puntos de trafico

---
+++ Components/ComponentsTraffiPuntos/Components/Section_Header.tsx
@@ -0,0 +1,30 @@
+import Feather from "@expo/vector-icons/Feather";
+import { View ,Text} from "react-native";
+import { TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+
+export default function Section_Header() {
+ return (
+    <View
+      style={{
+        paddingHorizontal: 10,
+        paddingTop: 40,
+        paddingBottom: 10,
+        backgroundColor: "#0F539C",
+      }}
+    >
+      <TouchableOpacity
+        onPress={() => router.back()}
+        style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+      >
+        <Feather name="arrow-left" size={28} color="#f8f9faa7" />
+        <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
+      </TouchableOpacity>
+      <View>
+        <Text style={{ fontSize: 24, fontWeight: "300", color: "white" }}>
+          Trafi-Puntos
+        </Text>
+      </View>
+    </View>
+  );
+}
