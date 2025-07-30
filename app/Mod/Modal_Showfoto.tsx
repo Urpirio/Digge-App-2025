e0c0a8cd242bd503cce1730ab60d035b8399a01d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-29 21:42:18 -0400

    Se agrego Modal para mostrar mas detalles de la foto, se planea no usar

---
+++ app/Mod/Modal_Showfoto.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useLocalSearchParams } from "expo-router";
+
+export default function Modal_Showfoto() {
+    const DataLocal = useLocalSearchParams();
+  return (
+  <SafeAreaProvider>
+    
+  </SafeAreaProvider>
+  )
+}
