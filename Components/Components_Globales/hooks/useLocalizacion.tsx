a83451c91ddd3f981b6e91fcf3c0769adc9d2403
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-29 21:44:36 -0400

    Se agreo el primer hook global es cual es de localizacion

---
+++ Components/Components_Globales/hooks/useLocalizacion.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import * as location from "expo-location";
+
+export const useLocalizacion = () => {
+  const [GeoMarker, setGeoMarker] = useState<any | null>([]);
+
+  const AgregarMarker = (E: { event: any }) => {
+    const { coordinate } = E.event.nativeEvent;
+    setGeoMarker([
+      ...GeoMarker,
+      {
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude,
+      },
+    ]);
+  };
+
+  const ConseguirUbicacion = async () => {
+    const { status } = await location.requestForegroundPermissionsAsync();
+
+    if (status) {
+      await location.getCurrentPositionAsync({});
+    }
+  };
+
+  const EliminarMarker = (L: { la: any; long: any }) => {
+    const DataPuntos = Array();
+    GeoMarker?.forEach((D: { latitude: any }) => {
+      if (D.latitude != L.la) {
+        DataPuntos.push(D);
+      }
+    });
+    setGeoMarker(DataPuntos);
+  };
+
+  return {
+    ConseguirUbicacion,
+    AgregarMarker,
+    GeoMarker,
+    setGeoMarker,
+    EliminarMarker,
+  };
+};
