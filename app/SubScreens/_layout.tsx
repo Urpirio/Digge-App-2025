942eb39d94f49b11b43d5251568c5b83815f8b61
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-29 21:42:34 -0400

    Se agrego un layout para las sub pantallas

---
+++ app/SubScreens/_layout.tsx
@@ -0,0 +1,50 @@
+import { Stack } from "expo-router";
+import { View } from "react-native";
+
+export default function _layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="ScreenAgregarVehiculos"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenCamaraFotos"
+        options={{
+          header: () => {
+            return <View style={{ height: 30, backgroundColor: "#0F539C" }} />;
+          },
+        }}
+      />
+      <Stack.Screen 
+        name="ScreenCamaraVideo" 
+        options={{
+          header: () => {
+            return <View style={{ height: 30, backgroundColor: "#0F539C" }} />;
+          },
+        }}
+      />
+      <Stack.Screen
+        name="ScreenReportarAveria"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenChatBotSoporte"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenDenunciarConductor"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenDetallesVehiculos"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenTraffiPuntos"
+        options={{ headerShown: false }}
+      />
+      
+    </Stack>
+  );
+}
