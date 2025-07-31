5af05b85b0a46c626f0219e54079c20da128a05e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-31 13:25:55 -0400

    Se elimaron cierto Stack los cuales dejaron de ser necesarios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,31 +28,39 @@ export default function _layout() {
           headerShown: false,
         }}
       />
+      <Stack.Screen name="SubScreens/ChatBotSoporte" options={{}} />
+
       <Stack.Screen
-        name="SubScreens/ChatBotSoporte"
+        name="Mod/Modal_EnviarUbicacion"
         options={{
-          
+          presentation: "transparentModal",
+          animation: "slide_from_bottom",
         }}
       />
-      <Stack.Screen name="Mod/Modal_MapReportesAverias" options={{
-        presentation:'transparentModal',
+      <Stack.Screen
+        name="Mod/Modal_InfoReporte"
+        options={{
+          animation: "slide_from_bottom",
+          presentation: "containedTransparentModal",
+        }}
+      />
+      <Stack.Screen
+        name="ScreensMap/ScreenMap_Navegacion"
+        options={{
+          animation: "slide_from_bottom",
+          presentation: "formSheet",
+        }}
+      />
+      <Stack.Screen
+        name="ScreensMap/ScreenMap_ReportesAverias"
+        options={{
+          presentation: "formSheet",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen name="ScreensMap/ScreenMap_Denuncias" options={{
+        presentation:'formSheet',
         animation:'slide_from_bottom'
-      }}/>
-      <Stack.Screen name="Mod/Modal_EnviarUbicacion" options={{
-        presentation: 'transparentModal',
-        animation:'slide_from_bottom'
-      }}/>
-      <Stack.Screen name="Mod/Modal_MapReportes" options={{
-        presentation:'transparentModal',
-        animation:'slide_from_bottom',
-        animationDuration:300
-      }} />
-      <Stack.Screen name="Mod/Modal_InfoReporte" options={{
-        animation:'slide_from_bottom',
-        presentation:'containedTransparentModal',
-        // sheetAllowedDetents:'fitToContents',
-        sheetExpandsWhenScrolledToEdge:true,
-        sheetGrabberVisible:true
       }}/>
     </Stack>
   );
