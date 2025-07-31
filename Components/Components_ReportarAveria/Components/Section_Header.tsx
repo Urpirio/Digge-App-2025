742b8dd76262a618c8896f145f745fc48ff4f2b2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-31 13:32:42 -0400

    Se elimino el Alert ya que estaba ocacionando un Bug raro

--- Components/Components_ReportarAveria/Components/Section_Header.tsx
+++ Components/Components_ReportarAveria/Components/Section_Header.tsx
@@ -7,7 +7,6 @@ export default function Section_Header() {
   const { AlertVolver } = useVolverAtras();
   useEffect(() => {
     BackHandler.addEventListener("hardwareBackPress", () => {
-      AlertVolver();
       return true;
     });
   }, []);
