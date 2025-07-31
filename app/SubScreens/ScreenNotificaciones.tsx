6fc143a02ad7566f827e56b6e84b6a68896a02b2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-31 13:28:06 -0400

    Se agrego un nueva pantalla  para las notifiaciones

---
+++ app/SubScreens/ScreenNotificaciones.tsx
@@ -0,0 +1,10 @@
+import Section_Header from "@/Components/Components_Notificaciones/Components/Section_Header";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function ScreenNotificaciones() {
+  return (
+    <SafeAreaProvider>
+        <Section_Header/>
+    </SafeAreaProvider>
+  )
+}
