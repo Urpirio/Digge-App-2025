9039577f895fcfa24a8cc3e36ccb312774f04c9b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-31 13:34:11 -0400

    Se agrego el Expo router A la seccion de emergencia

--- Components/Components_ScreenEmergencia/Components/Section_Emergencia.tsx
+++ Components/Components_ScreenEmergencia/Components/Section_Emergencia.tsx
@@ -1,12 +1,21 @@
-import { TouchableOpacity, View,Text } from 'react-native'
+import { router } from "expo-router";
+import { TouchableOpacity, View, Text } from "react-native";
 
 export default function Section_Emergencia() {
   return (
-   <View style={{paddingVertical:10,paddingHorizontal:20,gap:10}}>
-    <TouchableOpacity style={{padding:15,borderRadius:10,backgroundColor:'#EE2A24'}}>
-        <Text style={{textAlign:'center',fontSize:16,color:'white'}}>ACTIVAR EMERGENCIA</Text>
-    </TouchableOpacity>
-    <Text style={{textAlign:'center',color:'#737474ff'}}>Presiona para enviar tu ubicación a las autoridadeses y solicitar ayuda inmediata</Text>
-   </View>
-  )
+    <View style={{ paddingVertical: 10, paddingHorizontal: 20, gap: 10 }}>
+      <TouchableOpacity
+      onPress={()=>router.navigate('/Mod/Modal_EnviarUbicacion')}
+        style={{ padding: 15, borderRadius: 10, backgroundColor: "#EE2A24" }}
+      >
+        <Text style={{ textAlign: "center", fontSize: 16, color: "white" }}>
+          ACTIVAR EMERGENCIA
+        </Text>
+      </TouchableOpacity>
+      <Text style={{ textAlign: "center", color: "#737474ff" }}>
+        Presiona para enviar tu ubicación a las autoridadeses y solicitar ayuda
+        inmediata
+      </Text>
+    </View>
+  );
 }
