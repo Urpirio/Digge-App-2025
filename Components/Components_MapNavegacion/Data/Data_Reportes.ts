9e18a04e62ce8bdca23f17a1d0456d816b76e195
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-31 13:30:49 -0400

    Se agrego un array de datos

---
+++ Components/Components_MapNavegacion/Data/Data_Reportes.ts
@@ -0,0 +1,36 @@
+
+
+export const Data_Reportes = [
+    {
+        Titulo: 'Problemas con la calle',
+        Descripcion: 'Problemas con la calle, es totalmente de tierra y tiene hoyos super grandes',
+        longitud: -40.162651,
+        latitud: 17.8,
+        Imagen: 'https://i.pinimg.com/736x/78/bd/13/78bd13f8b0bbfe911f15831838952ac8.jpg',
+        Status: 'ReporteC',
+    },
+    {
+        Titulo: 'Problemas con la calle',
+        Descripcion: 'Problemas con la calle, es totalmente de tierra y tiene hoyos super grandes',
+        longitud: -69.941,
+        latitud: 18.457,
+        Imagen: 'https://i.pinimg.com/736x/78/bd/13/78bd13f8b0bbfe911f15831838952ac8.jpg',
+        Status: 'ReporteB',
+    },
+    {
+        Titulo: 'Problemas con la calle',
+        Descripcion: 'Problemas con la calle, es totalmente de tierra y tiene hoyos super grandes',
+        longitud: -80.162651,
+        latitud: 18,
+        Imagen: 'https://i.pinimg.com/736x/78/bd/13/78bd13f8b0bbfe911f15831838952ac8.jpg',
+        Status: 'ReporteC',
+    },
+    {
+        Titulo: 'Problemas con la calle',
+        Descripcion: 'Problemas con la calle, es totalmente de tierra y tiene hoyos super grandes',
+        longitud: -60.162651,
+        latitud: 18,
+        Imagen: 'https://i.pinimg.com/736x/78/bd/13/78bd13f8b0bbfe911f15831838952ac8.jpg',
+        Status: 'ReporteB',
+    }
+]
