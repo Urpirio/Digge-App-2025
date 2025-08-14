2c98405433b13b8392a512775e5d57f1e70111a8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-14 16:04:49 -0400

    Se hieron mejoras al login

--- app/ScreenL/ScreenLogin.tsx
+++ app/ScreenL/ScreenLogin.tsx
@@ -10,12 +10,12 @@ import {
 import { Link, router } from "expo-router";
 import { useEnviarDatos } from "@/Components/Components_Login/hooks/useEnviarDatos";
 import { useStyleLogin } from "@/Components/Components_Login/hooks/useStyleLogin";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function ScreenLogin() {
   const { InputEmail, setInputEmail, InputPass, setInputPass } =
     useEnviarDatos();
   const { IconPass, setIconPass, BtnIconPass } = useStyleLogin();
-
 
   return (
     <SafeAreaProvider>
@@ -28,16 +28,24 @@ export default function ScreenLogin() {
           position: "absolute",
           height: "100%",
           width: "100%",
-          paddingTop: 80,
+          paddingTop: 40,
           paddingHorizontal: 20,
-          gap: 30,
+          gap: 20,
         }}
       >
+        <View style={{width:'100%',alignItems:'flex-start'}}>
+          <TouchableOpacity
+            onPress={() => router.back()}
+            style={{ flexDirection: "row", alignItems: "center",borderRadius:'100%',padding:5,backgroundColor:'#0F539C'}}
+          >
+            <Feather name="arrow-left" size={28} color="white" />
+          </TouchableOpacity>
+        </View>
         <View
           style={{
             width: "100%",
             alignItems: "center",
-            gap: 30,
+            gap: 20,
             paddingHorizontal: 10,
           }}
         >
@@ -48,16 +56,44 @@ export default function ScreenLogin() {
           <Text style={{ fontSize: 20, fontWeight: "600" }}>
             Inicia sesión en tu cuenta
           </Text>
+
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              borderWidth: 1,
+              flexDirection: "row",
+              gap: 10,
+              paddingHorizontal: 15,
+              paddingVertical: 10,
+              borderRadius: 10,
+              borderColor: "#ced4da",
+            }}
+          >
+            <Image source={require("../../Assets/icon/Social icon.png")} />
+            <Text style={{ fontWeight: "500", color: "#6c757d" }}>
+              Inicia con Cuenta Única
+            </Text>
+          </TouchableOpacity>
         </View>
+        <View
+          style={{
+            borderWidth: 0.8,
+            width: "100%",
+            borderStyle: "dashed",
+            borderColor: "#ced4da",
+          }}
+        />
         <View style={{ gap: 10 }}>
-          <View style={{
+          <View
+            style={{
               backgroundColor: "#e9e9e97e",
               flexDirection: "row",
               justifyContent: "space-between",
               borderRadius: 10,
               alignItems: "center",
               padding: 5,
-            }}>
+            }}
+          >
             <TextInput
               value={InputEmail}
               onChangeText={setInputEmail}
@@ -65,8 +101,8 @@ export default function ScreenLogin() {
               placeholderTextColor={"#adb5bd"}
               style={{
                 fontSize: 16,
-                 width: "90%",
-                color:'gray'
+                width: "90%",
+                color: "gray",
               }}
             />
           </View>
@@ -87,7 +123,7 @@ export default function ScreenLogin() {
               secureTextEntry={IconPass}
               placeholder="Contraseña"
               placeholderTextColor={"#adb5bd"}
-              style={{ fontSize: 16, width: "90%",color:'gray' }}
+              style={{ fontSize: 16, width: "90%", color: "gray" }}
             />
             <TouchableOpacity onPress={() => setIconPass(!IconPass)}>
               <BtnIconPass />
@@ -102,8 +138,16 @@ export default function ScreenLogin() {
             Iniciar sesion
           </Text>
         </TouchableOpacity>
-        <Link style={{textAlign:'center',fontSize:15,fontWeight:'600',color:"#0F539C"}} href={'/'}>
-        Olvide mi Contraseña
+        <Link
+          style={{
+            textAlign: "center",
+            fontSize: 15,
+            fontWeight: "600",
+            color: "#0F539C",
+          }}
+          href={"/ScreenL/ScreenRecuperarP"}
+        >
+          Olvide mi Contraseña
         </Link>
       </View>
       <StatusBar barStyle={"dark-content"} />
