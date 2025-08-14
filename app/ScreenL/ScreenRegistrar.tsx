a33d4a643ca778ec63f9dca7710b5c1a32ad6566
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-14 16:07:33 -0400

    Se hicieron mejoras a la pantalla de Registrar usuario

--- app/ScreenL/ScreenRegistrar.tsx
+++ app/ScreenL/ScreenRegistrar.tsx
@@ -10,6 +10,8 @@ import {
 import { router } from "expo-router";
 import { useStyleRegistro } from "@/Components/Components_Registro/hooks/useStyleRegitro";
 import { useEnviarDatosR } from "@/Components/Components_Registro/hooks/useEnviarDatosR";
+import { KeyboardAvoidingView } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function ScreenRegistrar() {
   const {
@@ -38,21 +40,34 @@ export default function ScreenRegistrar() {
         style={{ objectFit: "contain", height: "100%", width: "100%" }}
         source={require("../../Assets/Backgrounds/Pagina de bienvenida.png")}
       />
-      <View
+      <KeyboardAvoidingView
+        behavior="padding"
         style={{
           position: "absolute",
-          height: "100%",
-          width: "100%",
-          paddingTop: 80,
+          flex: 1,
           paddingHorizontal: 20,
-          gap: 30,
+          gap: 20,
         }}
       >
+        <View style={{ paddingTop: 40, width: "100%",alignItems:'flex-start' }}>
+          <TouchableOpacity
+            onPress={() => router.back()}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              borderRadius: "100%",
+              padding: 5,
+              backgroundColor: "#0F539C",
+            }}
+          >
+            <Feather name="arrow-left" size={28} color="white" />
+          </TouchableOpacity>
+        </View>
         <View
           style={{
             width: "100%",
             alignItems: "center",
-            gap: 30,
+            gap: 15,
             paddingHorizontal: 10,
           }}
         >
@@ -61,9 +76,34 @@ export default function ScreenRegistrar() {
             source={require("../../Assets/Image/Logo.png")}
           />
           <Text style={{ fontSize: 20, fontWeight: "600" }}>
-            Crea tu cuenta
+            Crea tu cuenta usando
           </Text>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              borderWidth: 1,
+              flexDirection: "row",
+              gap: 10,
+              paddingHorizontal: 15,
+              paddingVertical: 10,
+              borderRadius: 10,
+              borderColor: "#ced4da",
+            }}
+          >
+            <Image source={require("../../Assets/icon/Social icon.png")} />
+            <Text style={{ fontWeight: "500", color: "#6c757d" }}>
+              Registrate con Cuenta Única
+            </Text>
+          </TouchableOpacity>
         </View>
+        <View
+          style={{
+            borderWidth: 0.8,
+            width: "100%",
+            borderStyle: "dashed",
+            borderColor: "#ced4da",
+          }}
+        />
         <View style={{ justifyContent: "center", gap: 10 }}>
           <View
             style={{
@@ -153,21 +193,20 @@ export default function ScreenRegistrar() {
               <BtnPass_B />
             </TouchableOpacity>
           </View>
-         
         </View>
-         <TouchableOpacity
-            onPress={() => router.navigate("/Screens/AScreenHome")}
-            style={{
-              padding: 12,
-              backgroundColor: "#0F539C",
-              borderRadius: 10,
-            }}
-          >
-            <Text style={{ textAlign: "center", color: "white", fontSize: 18 }}>
-              Registrarte
-            </Text>
-          </TouchableOpacity>
-      </View>
+        <TouchableOpacity
+          onPress={() => router.navigate("/Screens/AScreenHome")}
+          style={{
+            padding: 12,
+            backgroundColor: "#0F539C",
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ textAlign: "center", color: "white", fontSize: 18 }}>
+            Registrarte
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
       <StatusBar barStyle={"dark-content"} />
     </SafeAreaProvider>
   );
