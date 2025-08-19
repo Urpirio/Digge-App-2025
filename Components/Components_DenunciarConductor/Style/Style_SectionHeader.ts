812550009c37fc05b857c3e74a79343a09934f5e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 08:39:03 -0400

    Se agrego parte del estilo de la seccion Header

---
+++ Components/Components_DenunciarConductor/Style/Style_SectionHeader.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const Style_SectionHeader = StyleSheet.create({
+  ContainerHeader: {
+    paddingHorizontal: 10,
+    paddingTop: 40,
+    paddingBottom: 10,
+    backgroundColor: "#0F539C",
+  },
+  Text_Denuncia: {
+    fontSize: 24,
+    fontWeight: "300",
+    color: "white",
+  },
+  Btn_Volver: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+});
