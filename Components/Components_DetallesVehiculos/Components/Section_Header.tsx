5011ea35dceb172dbdc15d431570afe16c05d4aa
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 08:59:36 -0400

    Se movio el estilo internoa  a un documento externo

--- Components/Components_DetallesVehiculos/Components/Section_Header.tsx
+++ Components/Components_DetallesVehiculos/Components/Section_Header.tsx
@@ -1,25 +1,22 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import { router } from "expo-router";
+import { Style_SectionHeader } from "../Style/Style_SectionHeader";
 
 export default function Section_Header() {
   return (
-    <View
-      style={{
-        paddingHorizontal: 10,
-        paddingTop: 40,
-        paddingBottom: 10,
-        backgroundColor:'#0F539C'
-      }}
-    >
-      <TouchableOpacity 
-      onPress={()=>router.back()}
-      style={{flexDirection:'row',alignItems:'center',gap:5}}>
+    <View style={Style_SectionHeader.ContainerHeader}>
+      <TouchableOpacity
+        onPress={() => router.back()}
+        style={Style_SectionHeader.BtnVolver}
+      >
         <Feather name="arrow-left" size={28} color="#f8f9faa7" />
-        <Text style={{color:'#f8f9faa7'}}>Volver</Text>
+        <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
       </TouchableOpacity>
       <View>
-        <Text style={{fontSize:24,fontWeight:'300',color:'white'}}>Detalles de vehículo</Text>
+        <Text style={Style_SectionHeader.Text_BtnDetallesVehiculo}>
+          Detalles de vehículo
+        </Text>
       </View>
     </View>
   );
