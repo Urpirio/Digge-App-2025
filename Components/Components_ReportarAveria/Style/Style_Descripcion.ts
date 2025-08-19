e1f9ef1c02f32f392b5443dca9022ce4a47d4829
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 10:10:36 -0400

    Se agregaron los estilo de la seccion descripcion

---
+++ Components/Components_ReportarAveria/Style/Style_Descripcion.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const Style_Descripcion = StyleSheet.create({
+  Subcontainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+    borderRadius: 15,
+    borderColor: "#ced4da56",
+    boxShadow: "0px 0px 10px 0px #dbdbdb36",
+    backgroundColor: "#EBF5FB",
+  },
+  Text_Descripcion: {
+    paddingHorizontal: 15,
+    color: "#2874A6",
+    fontSize: 12,
+  },
+});
