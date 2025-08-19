e54d3b6f13dd5f2cd0e6d76e269ddedf4094071b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 08:38:24 -0400

    Se extrajo el estilo de la seccion y se coloco en un documento externo

--- Components/Components_DenunciarConductor/Components/Section_Header.tsx
+++ Components/Components_DenunciarConductor/Components/Section_Header.tsx
@@ -4,6 +4,7 @@ import { useVolverAtras } from "@/Components/Components_Globales/hooks/useVolver
 import { useCallback } from "react";
 import { BackHandler } from "react-native";
 import { useFocusEffect } from "expo-router";
+import { Style_SectionHeader } from "../Style/Style_SectionHeader";
 
 export default function Section_Header() {
   const { AlertVolver } = useVolverAtras();
@@ -15,25 +16,16 @@ export default function Section_Header() {
     }, [])
   );
   return (
-    <View
-      style={{
-        paddingHorizontal: 10,
-        paddingTop: 40,
-        paddingBottom: 10,
-        backgroundColor: "#0F539C",
-      }}
-    >
+    <View style={Style_SectionHeader.ContainerHeader}>
       <TouchableOpacity
         onPress={() => AlertVolver()}
-        style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+        style={Style_SectionHeader.Btn_Volver}
       >
         <Feather name="arrow-left" size={28} color="#f8f9faa7" />
         <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
       </TouchableOpacity>
       <View>
-        <Text style={{ fontSize: 24, fontWeight: "300", color: "white" }}>
-          Nueva denuncia
-        </Text>
+        <Text style={Style_SectionHeader.Text_Denuncia}>Nueva denuncia</Text>
       </View>
     </View>
   );
