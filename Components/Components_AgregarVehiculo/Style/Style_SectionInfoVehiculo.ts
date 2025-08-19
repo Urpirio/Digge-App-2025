dd948f0af6674e088d16ebd94f5f5dc514295983
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 07:15:50 -0400

    Se agrego un nuevo documento con los estilos de la seccion InfroVehiculos

---
+++ Components/Components_AgregarVehiculo/Style/Style_SectionInfoVehiculo.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const Style_SectionInfoVehiculo = StyleSheet.create({
+  Contenedor_ScrollView: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  Contanerdor_Formulario: {
+    gap: 10,
+    paddingBottom: 50,
+  },
+});
