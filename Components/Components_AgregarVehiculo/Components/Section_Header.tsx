beac96d378a406f4fc9aaab20bf1d0ba49864778
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 07:15:09 -0400

    Se movieron los estilos a un documento

--- Components/Components_AgregarVehiculo/Components/Section_Header.tsx
+++ Components/Components_AgregarVehiculo/Components/Section_Header.tsx
@@ -1,26 +1,20 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import { router } from "expo-router";
+import { Style_SectionHeader } from "../Style/Style_SectionHeader";
 
 export default function Section_Header() {
   return (
-    <View
-      style={{
-        paddingHorizontal: 10,
-        paddingTop: 40,
-        paddingBottom: 10,
-        backgroundColor: "#0F539C",
-      }}
-    >
+    <View style={Style_SectionHeader.ContainerHeader}>
       <TouchableOpacity
         onPress={() => router.back()}
-        style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+        style={Style_SectionHeader.BtnVolver}
       >
         <Feather name="arrow-left" size={28} color="#f8f9faa7" />
         <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
       </TouchableOpacity>
       <View>
-        <Text style={{ fontSize: 24, fontWeight: "300", color: "white" }}>
+        <Text style={Style_SectionHeader.Text_AgregarVehiculo}>
           Agregar vehículo
         </Text>
       </View>
