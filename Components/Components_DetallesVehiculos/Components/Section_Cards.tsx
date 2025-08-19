e4901de6a695372ff1a750f124358b8f1536f0b2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 08:59:28 -0400

    Se movio el estilo internoa  a un documento externo

--- Components/Components_DetallesVehiculos/Components/Section_Cards.tsx
+++ Components/Components_DetallesVehiculos/Components/Section_Cards.tsx
@@ -1,20 +1,19 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import Card_Principal from "../SubComponents/Card_Principal";
+import { Style_SectionCards } from "../Style/Style_SectionCard";
 
 export default function Section_Cards() {
   return (
-    <View style={{paddingHorizontal:10,paddingVertical:10}}>
-        <Card_Principal/>
-        <View style={{flexDirection:'row',justifyContent:'space-between',paddingVertical:10}}>
-            <TouchableOpacity style={{padding:10,width:'48%',borderRadius:10,justifyContent:'center',alignItems:'center',backgroundColor:'#22C55E'}}>
-                <Text style={{color:'white'}}>Renovar marbete</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{padding:10,width:'48%',borderRadius:10,justifyContent:'center',alignItems:'center',backgroundColor:'#1B95F4'}}>
-                <Text style={{color:'white'}}>Cambiar seguro</Text>
-            </TouchableOpacity>
-        </View>
-
-      
+    <View style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
+      <Card_Principal />
+      <View style={Style_SectionCards.ContainerBtns}>
+        <TouchableOpacity style={Style_SectionCards.btn_Renovar}>
+          <Text style={{ color: "white" }}>Renovar marbete</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Style_SectionCards.btn_CambiarSeguro}>
+          <Text style={{ color: "white" }}>Cambiar seguro</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
