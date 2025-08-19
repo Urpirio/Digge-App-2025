6ef12534ac4289edb13988365e5bb38b60d0ba1f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 10:09:49 -0400

    Se movio los estilos a un documento externo

--- Components/Components_ReportarAveria/Components/Section_Descripcion.tsx
+++ Components/Components_ReportarAveria/Components/Section_Descripcion.tsx
@@ -1,25 +1,14 @@
 import { View, Text } from "react-native";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Style_Descripcion } from "../Style/Style_Descripcion";
 
 export default function Section_Descripcion() {
   return (
     <View style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          borderWidth: 1,
-          paddingVertical: 15,
-          paddingHorizontal: 20,
-          justifyContent: "space-between",
-          borderRadius: 15,
-          borderColor:'#ced4da56',
-          boxShadow:'0px 0px 10px 0px #dbdbdb36',
-          backgroundColor:'#EBF5FB'
-        }}
-      >
+      <View style={Style_Descripcion.Subcontainer}>
         <MaterialCommunityIcons name="information" size={30} color="#3498DB" />
-        <Text style={{ paddingHorizontal:15,color:'#2874A6',fontSize:12 }}>
+        <Text style={Style_Descripcion.Text_Descripcion}>
           Reporta daños en vías, señales o infraestructura vial para que las
           autoridades puedan tomar acción y mantener las carreteras seguras para
           todos.
