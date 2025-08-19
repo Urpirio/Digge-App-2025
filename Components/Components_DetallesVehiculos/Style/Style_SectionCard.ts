1b94b4d29988ee64c19718943f90c571f5ab49ec
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 09:00:15 -0400

    Se agrego el estilo de la seccion en un documento

---
+++ Components/Components_DetallesVehiculos/Style/Style_SectionCard.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const Style_SectionCards = StyleSheet.create({
+  ContainerBtns: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+  btn_Renovar: {
+    padding: 10,
+    width: "48%",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#22C55E",
+  },
+  btn_CambiarSeguro: {
+    padding: 10,
+    width: "48%",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1B95F4",
+  },
+});
