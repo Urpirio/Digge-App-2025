7122f88188718a9065aa55670c4c2a03c26d0e3c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 08:38:16 -0400

    Se extrajo el estilo de la seccion y se coloco en un documento externo

--- Components/Components_DenunciarConductor/Components/Section_Descripcion.tsx
+++ Components/Components_DenunciarConductor/Components/Section_Descripcion.tsx
@@ -1,27 +1,17 @@
-import { View,Text } from "react-native";
+import { View, Text } from "react-native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Style_SectionDescripcion } from "../Style/Style_SectionDescripcion";
 
 export default function Section_Descripcion() {
   return (
-       <View style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          borderWidth: 1,
-          paddingVertical: 15,
-          paddingHorizontal: 20,
-          justifyContent: "space-between",
-          borderRadius: 15,
-          borderColor:'#ced4da56',
-          boxShadow:'0px 0px 10px 0px #dbdbdb36',
-          backgroundColor:'#EBF5FB'
-        }}
-      >
+    <View style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
+      <View style={Style_SectionDescripcion.SubContainer_Descripcion}>
         <MaterialCommunityIcons name="information" size={30} color="#3498DB" />
-        <Text style={{ paddingHorizontal:15,color:'#2874A6',fontSize:12 }}>
-          Reporta el mal comportamiento de otros conductores para mejorar la seguridad vial. Tu denuncia será evaluada.
+        <Text style={Style_SectionDescripcion.Text_Descripcion}>
+          Reporta el mal comportamiento de otros conductores para mejorar la
+          seguridad vial. Tu denuncia será evaluada.
         </Text>
       </View>
     </View>
-  )
+  );
 }
