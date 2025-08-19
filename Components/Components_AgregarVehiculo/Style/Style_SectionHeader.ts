40c9927f3b0cea6ede8c0d343dd9796c5611ccaa
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 07:15:42 -0400

    Se agrego un nuevo documento con los estilos de la seccion  HEader

---
+++ Components/Components_AgregarVehiculo/Style/Style_SectionHeader.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const Style_SectionHeader = StyleSheet.create({
+  ContainerHeader: {
+    paddingHorizontal: 10,
+    paddingTop: 40,
+    paddingBottom: 10,
+    backgroundColor: "#0F539C",
+  },
+  BtnVolver: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  Text_AgregarVehiculo: {
+    fontSize: 24,
+    fontWeight: "300",
+    color: "white",
+  },
+});
