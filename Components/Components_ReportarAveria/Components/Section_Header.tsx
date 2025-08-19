070691ec19f085d12956043675601775aedf1f91
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-19 10:10:17 -0400

    Se movio los estilo a un documento externo

--- Components/Components_ReportarAveria/Components/Section_Header.tsx
+++ Components/Components_ReportarAveria/Components/Section_Header.tsx
@@ -2,6 +2,7 @@ import { View, TouchableOpacity, Text, BackHandler } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import { useVolverAtras } from "../../Components_Globales/hooks/useVolverAtras";
 import { useEffect } from "react";
+import { Style_Header } from "../Style/Style_Header";
 
 export default function Section_Header() {
   const { AlertVolver } = useVolverAtras();
@@ -11,25 +12,16 @@ export default function Section_Header() {
     });
   }, []);
   return (
-    <View
-      style={{
-        paddingHorizontal: 10,
-        paddingTop: 40,
-        paddingBottom: 10,
-        backgroundColor: "#0F539C",
-      }}
-    >
+    <View style={Style_Header.ContainerHeader}>
       <TouchableOpacity
         onPress={() => AlertVolver()}
-        style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+        style={Style_Header.BtnVolver}
       >
         <Feather name="arrow-left" size={28} color="#f8f9faa7" />
         <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
       </TouchableOpacity>
       <View>
-        <Text style={{ fontSize: 24, fontWeight: "300", color: "white" }}>
-          Reportar avería
-        </Text>
+        <Text style={Style_Header.Text_resporte}>Reportar avería</Text>
       </View>
     </View>
   );
