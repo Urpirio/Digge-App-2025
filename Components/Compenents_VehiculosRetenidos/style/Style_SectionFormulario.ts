dba89fa9ae6c58154cbaf7e9f9a13409bbced3a6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-20 09:40:30 -0400

    Se agregaron los estilos de la section del formulario

---
+++ Components/Compenents_VehiculosRetenidos/style/Style_SectionFormulario.ts
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export const Style_SectionFormulario = StyleSheet.create({
+  Text_Input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 55,
+    fontSize: 16,
+    borderColor: "#D1D0D0",
+    backgroundColor: "#efeded44",
+  },
+  Container_Titulos: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    width: "100%",
+    paddingVertical: 10,
+  },
+  Btn_SelecionarUbicacion: {
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 18,
+    paddingHorizontal: 15,
+    gap: 5,
+    borderRadius: 10,
+    borderColor: "#D1D0D0",
+    backgroundColor: "#efeded44",
+  },
+  Btn_Enviar: {
+    padding: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#0F539C",
+    flexDirection: "row",
+    gap: 5,
+  },
+  Container_Pickers: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#D1D0D0",
+    backgroundColor: "#efeded44",
+  },
+  BtnFechaHora: {
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    gap: 5,
+    borderRadius: 10,
+    borderColor: "#D1D0D0",
+    backgroundColor: "#efeded44",
+  },
+});
