4be8857a2bc7bc4ae40860e86ba9106546818cc4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-20 15:46:05 -0400

    Se agrego el Data Array de Actualidad

---
+++ Components/Components_ScreenHome/Data/Data_Actulidad.tsx
@@ -0,0 +1,15 @@
+
+export const Data_Actualidad = [
+    {
+        TituloDS: "Prueba de novedades",
+    },
+    {
+        TituloDS: "Prueba de novedades 2",
+    },
+    {
+        TituloDS: "Prueba de novedades 3",
+    },
+    {
+        TituloDS: "Prueba de novedades 4",
+    },
+]
