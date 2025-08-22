457983e304cbae66b82cd43f2126616c75f65d69
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-22 14:35:25 -0400

    Se agrego el segundo formulario

---
+++ Components/Components_Registro/Components/Section_Form2.tsx
@@ -0,0 +1,63 @@
+import { Dispatch, SetStateAction } from "react";
+import { View, TextInput } from "react-native";
+
+interface Tools {
+  InputNumero1: string | undefined;
+  InputNumero2: string | undefined;
+  setInputNumero1: Dispatch<SetStateAction<string | undefined>>;
+  setInputNumero2: Dispatch<SetStateAction<string | undefined>>;
+}
+
+export default function Section_Form2({
+  InputNumero1,
+  InputNumero2,
+  setInputNumero1,
+  setInputNumero2,
+}: Tools) {
+  return (
+    <View style={{ justifyContent: "center", gap: 10 }}>
+      <View
+        style={{
+          backgroundColor: "#e9e9e97e",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderRadius: 10,
+          padding: 5,
+        }}
+      >
+        <TextInput
+          value={InputNumero1}
+          onChangeText={setInputNumero1}
+          placeholder="Numero de telefono "
+          placeholderTextColor={"#adb5bd"}
+          style={{
+            fontSize: 16,
+            width: "100%",
+          }}
+        />
+      </View>
+      <View
+        style={{
+          backgroundColor: "#e9e9e97e",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderRadius: 10,
+          padding: 5,
+        }}
+      >
+        <TextInput
+          value={InputNumero2}
+          onChangeText={setInputNumero2}
+          placeholder="Numero de telefono"
+          placeholderTextColor={"#adb5bd"}
+          style={{
+            fontSize: 16,
+            width: "100%",
+          }}
+        />
+      </View>
+    </View>
+  );
+}
