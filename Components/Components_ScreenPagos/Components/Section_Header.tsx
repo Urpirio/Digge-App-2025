5897c566490c76fcf966102471b2dae3d430f3f0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 21:04:45 -0400

    Se modifico un oath

--- Components/Components_ScreenPagos/Components/Section_Header.tsx
+++ Components/Components_ScreenPagos/Components/Section_Header.tsx
@@ -32,7 +32,7 @@ export default function Section_Header({
         }}
       >
         <TouchableOpacity
-          onPress={() => router.navigate("/Screens/ScreenPerfil")}
+          onPress={() => router.navigate("/SubScreens/ScreenPerfil")}
           style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
         >
           <Image
