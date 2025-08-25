d82a0c7ca13a3e9a76855dbdeaf2ef5709118de8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 21:04:36 -0400

    Pasar datos de infracciones como apoyo a las tarjetas del panel
    Section_Dashboard ahora recibe una propiedad Data y la pasa a Card_ResumenInfracciones y Card_InfraccionesPendientes. Este cambio permite que estos componentes se representen según los datos de infracciones proporcionados.

--- Components/Components_ScreenPagos/Components/Section_Dashboard.tsx
+++ Components/Components_ScreenPagos/Components/Section_Dashboard.tsx
@@ -1,17 +1,32 @@
 import { View, Text, ScrollView } from "react-native";
 import Card_ResumenInfracciones from "../SubComponents/Card_ResumenInfracciones";
 import Card_InfraccionesPendientes from "../SubComponents/Card_InfraccionesPendientes";
+import {
+  FiltroPagadas,
+  FiltrosPendientes,
+  Objectos_Infracciones,
+} from "../hooks/useGet_Infracciones";
 
-export default function Section_Dashboard() {
+export default function Section_Dashboard(Tools: {
+  Data: Array<Objectos_Infracciones> | undefined;
+  // Data_Pendientes: FiltrosPendientes | undefined;
+  // Data_Pagadas: FiltroPagadas | undefined;
+}) {
+  const { Data } = Tools;
+
   return (
     <ScrollView style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
-      <Card_ResumenInfracciones />
+      <Card_ResumenInfracciones
+        Data={Data}
+        // Data_Pagadas={Data_Pagadas}
+        // Data_Pendientes={Data_Pendientes}
+      />
       <View style={{ gap: 10, paddingTop: 10, paddingBottom: 100 }}>
         <Text style={{ fontSize: 20, fontWeight: "600" }}>
           Infracciones Pendientes
         </Text>
         <View>
-          <Card_InfraccionesPendientes />
+          <Card_InfraccionesPendientes Data={Data} />
         </View>
       </View>
     </ScrollView>
