16002147226b0c5b4f3eb73a9b4e3639ad4c16a9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 21:03:53 -0400

    Se refactorizo

--- Components/Components_ScreenHome/SubComponents/Card_AccionesRegistro.tsx
+++ Components/Components_ScreenHome/SubComponents/Card_AccionesRegistro.tsx
@@ -1,63 +1,59 @@
 import { Text, View } from "react-native";
+import { AccionRegistro } from "../Data/Data_GeneralCard";
+import { Data_GeneralCard } from "../Data/Data_GeneralCard";
 
-interface Content {
-  bgIconColor: String;
-  Icon: any;
-  Titulo: String;
-  subTitulo: String;
-  hora: String;
-  Stat: String;
-  ColorStat:String;
-}
-
-export default function Card_AccionesRegistro({
-  bgIconColor,
-  Icon,
-  Titulo,
-  subTitulo,
-  hora,
-  Stat,
-  ColorStat,
-}: Content) {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        borderWidth: 1,
-        paddingHorizontal: 8,
-        paddingVertical: 5,
-        borderRadius: 15,
-        justifyContent: "space-between",
-        gap: 5,
-        alignItems: "center",
-        backgroundColor: "#fcfcfcff",
-        borderColor: "#EBEBEB",
-      }}
-    >
-      <View style={{flexDirection:'row',alignItems:'center',gap:5}}>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            height: 45,
-            width: 45,
-            borderRadius: 10,
-            backgroundColor: `#${bgIconColor}`,
-          }}
-        >
-          {Icon}
+export default function Card_AccionesRegistro(Tools: {
+  Data: Array<AccionRegistro> ;
+}) {
+  const { Data } = Tools;
+  const Cars = Data_GeneralCard.map((D) => {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          borderWidth: 1,
+          paddingHorizontal: 8,
+          paddingVertical: 5,
+          borderRadius: 15,
+          justifyContent: "space-between",
+          gap: 5,
+          alignItems: "center",
+          backgroundColor: "#fcfcfcff",
+          borderColor: "#EBEBEB",
+        }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              height: 45,
+              width: 45,
+              borderRadius: 10,
+              backgroundColor: `#${D?.bgIconColor}`,
+            }}
+          >
+            {D?.Icon}
+          </View>
+          <View>
+            <Text style={{ fontWeight: "600", fontSize: 16 }}>{D?.Titulo}</Text>
+            <Text style={{ fontSize: 12, color: "gray" }}>{D?.subTitulo}</Text>
+            <Text style={{ color: "#c3c3c3ff" }}>4h</Text>
+          </View>
         </View>
-        <View>
-          <Text style={{ fontWeight: "600", fontSize: 16 }}>{Titulo}</Text>
-          <Text style={{ fontSize: 12, color: "gray" }}>{subTitulo}</Text>
-          <Text style={{ color: "#c3c3c3ff" }}>4h</Text>
+        <View style={{ height: "100%" }}>
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: "600",
+              color: `#${D?.ColorStat}`,
+            }}
+          >
+            {D?.Stat}
+          </Text>
         </View>
       </View>
-      <View style={{ height: "100%" }}>
-        <Text style={{ fontSize: 12, fontWeight: "600", color: `#${ColorStat}` }}>
-          {Stat}
-        </Text>
-      </View>
-    </View>
-  );
+    );
+  });
+  return Cars;
 }
