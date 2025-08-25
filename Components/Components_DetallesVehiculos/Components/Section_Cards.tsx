311a9c85446c584220de25bf91c7b60f07aba033
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 20:59:02 -0400

    Agregar navegación al modal de funciones no disponibles
    Se añadieron controladores onPress a los botones "Renovar marbete" y "Cambiar seguro" para navegar a un modal que indica que la función solicitada no está disponible. Esto proporciona información a los usuarios sobre las funciones que aún no se han implementado.

--- Components/Components_DetallesVehiculos/Components/Section_Cards.tsx
+++ Components/Components_DetallesVehiculos/Components/Section_Cards.tsx
@@ -1,16 +1,45 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import Card_Principal from "../SubComponents/Card_Principal";
 import { Style_SectionCards } from "../Style/Style_SectionCard";
+import { router } from "expo-router";
 
 export default function Section_Cards() {
   return (
     <View style={{ paddingHorizontal: 10, paddingVertical: 10 }}>
       <Card_Principal />
       <View style={Style_SectionCards.ContainerBtns}>
-        <TouchableOpacity style={Style_SectionCards.btn_Renovar}>
+        <TouchableOpacity
+          onPress={() => {
+            router.navigate({
+              pathname: "/Mod/Modal_DatosEnviados",
+              params: {
+                descripcion:
+                  "La función solicitada no se encuentra disponible en este momento. Estamos trabajando para habilitarla lo antes posible.",
+                titulo: "Función actualmente no disponible",
+                Error: "true",
+                tipo_s: "otro reporte",
+              },
+            });
+          }}
+          style={Style_SectionCards.btn_Renovar}
+        >
           <Text style={{ color: "white" }}>Renovar marbete</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={Style_SectionCards.btn_CambiarSeguro}>
+        <TouchableOpacity
+          onPress={() => {
+            router.navigate({
+              pathname: "/Mod/Modal_DatosEnviados",
+              params: {
+                descripcion:
+                  "La función solicitada no se encuentra disponible en este momento. Estamos trabajando para habilitarla lo antes posible.",
+                titulo: "Función actualmente no disponible",
+                Error: "true",
+                tipo_s: "otro reporte",
+              },
+            });
+          }}
+          style={Style_SectionCards.btn_CambiarSeguro}
+        >
           <Text style={{ color: "white" }}>Cambiar seguro</Text>
         </TouchableOpacity>
       </View>
