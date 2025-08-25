458041ab208cc643f9cea61303d61c33bd0accb8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 21:03:20 -0400

    Se modifico el path

--- Components/Components_ScreenHome/Components/Section_User.tsx
+++ Components/Components_ScreenHome/Components/Section_User.tsx
@@ -57,7 +57,7 @@ export default function Section_User(Tools: {
         </View>
 
         <TouchableOpacity
-          onPress={() => router.navigate("/Screens/ScreenPerfil")}
+          onPress={() => router.navigate("/SubScreens/ScreenPerfil")}
           style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
         >
           <Image
