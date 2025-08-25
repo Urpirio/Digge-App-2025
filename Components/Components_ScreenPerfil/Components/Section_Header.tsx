42d36f6cc63e9fbaf9521773f2e96e82ea627a60
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-24 21:06:22 -0400

    Agregar botón Atrás al encabezado de la sección de perfil
    Se introdujo un botón "Volver" con un icono de flecha en el componente Section_Header, actualizando el diseño para usar la alineación con espacio entre elementos. Esto mejora la navegación al permitir a los usuarios regresar a la pantalla anterior.

--- Components/Components_ScreenPerfil/Components/Section_Header.tsx
+++ Components/Components_ScreenPerfil/Components/Section_Header.tsx
@@ -8,13 +8,28 @@ export default function Section_Header() {
       style={{
         backgroundColor: "#0F539C",
         flexDirection: "row",
-        justifyContent: "center",
+        justifyContent: "space-between",
         paddingVertical: 10,
       }}
     >
+      <View style={{width: "30%",paddingLeft:10}}>
+        <TouchableOpacity
+          onPress={() => router.back()}
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            
+          }}
+        >
+          <Feather name="arrow-left" size={28} color="#f8f9faa7" />
+          <Text style={{ color: "#f8f9faa7" }}>Volver</Text>
+        </TouchableOpacity>
+      </View>
       <Text style={{ fontSize: 20, color: "white", fontWeight: "500" }}>
         Perfil
       </Text>
+      <View style={{ width: "30%" }}></View>
     </View>
   );
 }
